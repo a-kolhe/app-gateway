4a537b9174034cf652330c635c3c97c16bdca221
aadeshkolhe <aadeshkolhe>
2025-07-08 22:16:04 +0530

    working changes

--- src/main/java/com/working/app_gateway/filter/AuthenticationFilter.java
+++ src/main/java/com/working/app_gateway/filter/AuthenticationFilter.java
@@ -1,16 +1,28 @@
 package com.working.app_gateway.filter;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 
 import com.working.app_gateway.util.JwtUtil;
 import com.working.app_gateway.util.RouteValidator;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 
 
 @Component
+@Slf4j
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
 	@Autowired
 	private RouteValidator validator;
@@ -19,6 +31,8 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
 //    private RestTemplate template;
 	@Autowired
 	private JwtUtil jwtUtil;
+
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AuthenticationFilter.class);
 
 	public AuthenticationFilter() {
 		super(Config.class);
@@ -42,14 +56,31 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
 //                    template.getForObject("http://IDENTITY-SERVICE//validate?token" + authHeader, String.class);
 					jwtUtil.validateToken(authHeader);
 
-				} catch (Exception e) {
-					System.out.println("invalid access...!");
-					throw new RuntimeException("un authorized access to application");
+				} catch (ExpiredJwtException  e) {
+				    log.warn("JWT expired: {}", e.getMessage());
+//					System.out.println("invalid access...!");
+				    return unauthorizedResponse(exchange, "Token expired");
+				}catch (JwtException  e2) {
+				    log.warn("JWT invalid: {}", e2.getMessage());
+				    return unauthorizedResponse(exchange, "Invalid token");
+
+					// TODO: handle exception
+				}catch (Exception e3) {
+					log.error("Exception : {}",e3.getMessage(),e);
 				}
 			}
 			return chain.filter(exchange);
 		});
 	}
+	
+	private Mono<Void> unauthorizedResponse(ServerWebExchange exchange, String message) {
+	    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+	    DataBuffer buffer = exchange.getResponse().bufferFactory()
+	        .wrap(("{\"error\":\"" + message + "\"}").getBytes(StandardCharsets.UTF_8));
+	    exchange.getResponse().getHeaders().add("Content-Type", "application/json");
+	    return exchange.getResponse().writeWith(Mono.just(buffer));
+	}
+
 
 	public static class Config {
 
